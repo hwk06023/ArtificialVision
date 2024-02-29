f1fea86dbc095ad1aeee89d84f496a6a6ace3850
Youngseok Kim <ericsky2850@gmail.com>
2024-02-29 15:45:10 +0900

    FEAT Download from url function with progressbar for utils.

---
+++ artificialvision/utils/download_from_url.py
@@ -0,0 +1,16 @@
+from tqdm import tqdm
+import urllib.request
+
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_from_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(
+            url, filename=output_path, reporthook=t.update_to)
