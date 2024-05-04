709b13d493208c46519315beb3b3d182123f4632
hwk06023 <hwk06023@gmail.com>
2024-05-04 17:07:40 +0900

    update TODO

--- artificialvision/SuperResolution.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-# Load the low resolution image
-lr_image = cv2.imread("path_to_lr_image.jpg")
-
-hr_image = cv2.resize(lr_image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-
-# Save the high resolution image
-cv2.imwrite("path_to_hr_image.jpg", hr_image)
